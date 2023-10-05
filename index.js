c59b8631dd1d177c9e0769bdc306717cf5ae6e3d
Lennythecoder <69860127+Lennythecoder@users.noreply.github.com>
2023-10-04 19:46:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ async function runExample1(
  x[8] = Document.getElementById('box9').value;
  x[9] = Document.getElementById('box10').value;
  x[10] = Document.getElementById('box11').value;
-
+ let tensorX = new onnx.Tensor(x, 'float32', (1, 11)
 )
