399a3bdc0e97b392b211f1bd20863bb13f40182d
Lennythecoder <69860127+Lennythecoder@users.noreply.github.com>
2023-10-24 14:04:14 -0500

    Update index.js

--- XGBoost/index.js
+++ XGBoost/index.js
@@ -1,6 +1,10 @@
-// function that runs when a button is pressed
-async function runExample1() {
+
+async function runExample(){
+
+  // var x = new Float32Array(1, 11);
+
   var x = [];
+  
   x[0] = document.getElementById('box1').value;
   x[1] = document.getElementById('box2').value;
   x[2] = document.getElementById('box3').value;
@@ -12,22 +16,28 @@ async function runExample1() {
   x[8] = document.getElementById('box9').value;
   x[9] = document.getElementById('box10').value;
   x[10] = document.getElementById('box11').value;
-  
-  let tensorX = new ort.Tensor(x, 'float32', [1, 11]);
-  let feeds = { 'input_name_in_model': tensorX }; 
-  
-  let session = await ort.InferenceSession('xgboost_winequality_ort.onnx'); 
+
+
+  let tensorX = new ort.Tensor('float32', x, [1, 11]);
+
+  let feeds = {float_input: tensorX};
+
+  let session = await ort.InferenceSession.create('xgboost_WineQuality_ort.onnx');
+
   let result = await session.run(feeds);
+
   let outputData = result.variable.data;
+
+  outputData = parseFloat(outputData).toFixed(2)
   
-  outputData = parseFloat(outputData).toFixed(2); // Correct variable name and parsing.
   let predictions = document.getElementById('predictions');
-  predictions.innerHTML = `<hr> Got an Output Tensor with values: <br/>
-    <table>
-      <tr>
-        <td>Rating of Wine Quality</td>
-        <td id="td0">${outputData}</td> <!-- Correct variable name -->
-      </tr>
-    </table>
-  `;
+
+  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
+  <table>
+    <tr>
+      <td> Rating of Wine Quality </td>
+      <td id="td0"> ${outputData} </td>
+    </tr>
+  </table>`;
+  
 }
